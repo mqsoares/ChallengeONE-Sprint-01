b3fd0035d80609c0aa4808d3f3586845fb4f0e6b
mqsoares <mqseraos@gmail.com>
2022-12-20 13:44:52 -0300

    Create Function Decrypt

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,6 +6,7 @@ const txtResponse = document.querySelector('.txtResponse');
 const txtArea = document.querySelector('.txtArea');
 
 btnEncrypt.addEventListener('click', encrypt)
+btnDecrypt.addEventListener('click', decrypt)
 
 function encrypt() {
     if( txtArea.value.length > 0) {
@@ -15,6 +16,20 @@ function encrypt() {
                                 .replaceAll('a','ai')
                                 .replaceAll('o','ober')
                                 .replaceAll('u','ufat');
+        txtResponse.innerHTML = response;
+        areaResponse.classList.add('response');
+        return response;
+    }
+}
+
+function decrypt() {
+    if (txtArea.value.length > 0) {
+        const txtEncypt = txtArea.value;
+        const response = txtEncypt.replaceAll('enter', 'e')
+                                    .replaceAll('imes', 'i')
+                                    .replaceAll('ai','a')
+                                    .replaceAll('ober','o')
+                                    .replaceAll('ufat','u');
         txtResponse.innerHTML = response;
         areaResponse.classList.add('response');
         return response;
